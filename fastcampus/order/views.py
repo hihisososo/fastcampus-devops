70d6b3c2ad8578203b70861d2dc62f3ee4e1061a
hihisososo <hihisososo@naver.com>
2022-06-19 20:06:43 +0900

    CH02_09. (백엔드) Models 제작

--- fastcampus/order/views.py
+++ fastcampus/order/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from order.models import Shop,Menu,Order,Orderfood
-from order.serializers import ShopSerializer
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
+from .models import Shop
+from .serializers import ShopSerializer
 
+
+@csrf_exempt
 def shop(request):
-    if request.method == 'GET':
-        shop = Shop.objects.all()
-        serializer = ShopSerializer(shop, many=True)
-        return JsonResponse(serializer.data, safe=False)
+  if request.method == 'GET':
+    shop = Shop.objects.all()
+    serializer = ShopSerializer(shop, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
-#     elif request.method =='POST':
+  elif request.method == 'POST':
+    data = JSONParser().parse(request)
+    serializer = ShopSerializer(data=data)
+    if (serializer.is_valid()):
+      serializer.save()
+      return JsonResponse(serializer.data, status=201)
+    return JsonResponse(serializer.errors, status=400)
+
