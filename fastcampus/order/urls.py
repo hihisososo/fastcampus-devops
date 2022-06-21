76e5cd13d6553ceab6635347ebdd5013e82facf9
hihisososo <hihisososo@naver.com>
2022-06-21 20:01:52 +0900

    2.11 (백엔드) Order 백엔드 개발2

--- fastcampus/order/urls.py
+++ fastcampus/order/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('shops/', views.shop, name="shop"),
-    path('menus/<int:shop>', views.menu, name="menu")
+    path('menus/<int:shop>', views.menu, name="menu"),
+    path('order/', views.order, name="order")
 ]
--- fastcampus/order/views.py
+++ fastcampus/order/views.py
@@ -1,9 +1,9 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
-from .models import Menu
+from .models import Menu, Order
 from .models import Shop
 from .serializers import MenuSerializer
 from .serializers import ShopSerializer
@@ -12,10 +12,6 @@ from .serializers import ShopSerializer
 @csrf_exempt
 def shop(request):
   if request.method == 'GET':
-    # shop = Shop.objects.all()
-    # serializer = ShopSerializer(shop, many=True)
-    # return JsonResponse(serializer.data, safe=False)
-
     shop = Shop.objects.all();
     return render(request, 'order/shop_list.html', {'shop_list': shop});
 
@@ -34,7 +30,7 @@ def menu(request, shop):
     menu = Menu.objects.filter(shop=shop)
     # serializer = MenuSerializer(menu, many=True)
     # return JsonResponse(serializer.data, safe=False)
-    return render(request, 'order/menu_list.html',   {'menu_list': menu});
+    return render(request, 'order/menu_list.html', {'menu_list': menu, 'shop': shop});
 
   elif request.method == 'POST':
     data = JSONParser().parse(request)
@@ -43,3 +39,28 @@ def menu(request, shop):
       serializer.save()
       return JsonResponse(serializer.data, status=201)
     return JsonResponse(serializer.errors, status=400)
+
+
+from django.utils import timezone
+
+
+@csrf_exempt
+def order(request):
+  if request.method == 'POST':
+    address = request.POST['address']
+    shop = request.POST['shop']
+    food_list = request.POST.getlist('menu')
+    order_date = timezone.now()
+
+    shop_item = Shop.objects.get(pk=shop)
+
+    shop_item.order_set.create(address=address, order_date=order_date, shop=int(shop))
+
+    order_item = Order.objects.get(pk=shop_item.order_set.latest('id').id)
+    for food in food_list:
+      order_item.orderfood_set.create(food_name=food)
+
+    return HttpResponse(status=200)
+  elif request.method == 'GET':
+    order_list = Order.objects.all()
+    return render(request, 'order/order_list.html', {'order_list': order_list});
