a2bd797966b4dcb37b266bb857144562b6ca092f
lyj <lyj@diquest.com>
2022-07-02 08:47:00 +0900

    CH02_14. (백엔드) Boss 백엔드 개발2

--- fastcampus/boss/urls.py
+++ fastcampus/boss/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from boss import views
 
 urlpatterns = [
-  path('timeinput/', views.timeinput, name='timeinput')
+  path('orders/<int:shop>', views.order_list, name='order_list'),
+  path('timeinput', views.time_input, name='timeinput')
 ]
--- fastcampus/boss/views.py
+++ fastcampus/boss/views.py
@@ -6,13 +6,18 @@ from order.models import Order
 
 
 @csrf_exempt
-def timeinput(request):
+def order_list(request, shop):
   if request.method == 'GET':
-    order_list = Order.objects.all()
+    order_list = Order.objects.filter(shop=shop)
     return render(request, 'boss/order_list.html', {'order_list': order_list})
 
-  elif request.method == 'POST':
+@csrf_exempt
+def time_input(request):
+  if request.method == 'POST':
     order_item = Order.objects.get(pk=request.POST['order_id'])
+    shop = order_item.shop.id
     order_item.estimate_time = int(request.POST['estimated_time'])
     order_item.save()
-    return render(request, 'boss/success.html')
+    return render(request, 'boss/success.html', {'shop' : int(shop)})
+  else:
+    return HttpResponse(status=404)
