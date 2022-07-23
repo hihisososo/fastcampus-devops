2267ac2966dbefddb7cb185c4fcf30c9c759857f
lyj <lyj@diquest.com>
2022-07-23 18:20:21 +0900

    ### 2.19 (인프라) ALB 에 인스턴스 연결하여 웹서비스 실행하기

--- fastcampus/fastcampus/settings.py
+++ fastcampus/fastcampus/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = "@o&=0wd$n3undsjh@$r0p6fsqslkh5bl8mm-t@3@ritinkrd42"
 DEBUG = True
 
 ALLOWED_HOSTS = ['ec2-3-34-190-219.ap-northeast-2.compute.amazonaws.com',
-                 '172.31.43.201']
+                 'elb-1168980036.ap-northeast-2.elb.amazonaws.com']
 
 
 # Application definition
