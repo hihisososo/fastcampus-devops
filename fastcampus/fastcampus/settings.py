5b8e8b9d9b5a9055fad50c2d808392ca346a1824
lyj <lyj@diquest.com>
2022-08-03 20:54:38 +0900

    ALLOWED_HOSTS 변경

--- fastcampus/fastcampus/settings.py
+++ fastcampus/fastcampus/settings.py
@@ -41,7 +41,8 @@ SECRET_KEY = "@o&=0wd$n3undsjh@$r0p6fsqslkh5bl8mm-t@3@ritinkrd42"
 DEBUG = True
 
 ALLOWED_HOSTS = ['ec2-3-35-11-206.ap-northeast-2.compute.amazonaws.com',
-                 '172.31.43.201']
+                 '172.31.43.201',
+                 '172.31.47.82']
 
 
 # Application definition
